5b1de541311a299a2065768e0f8895347386fa71
David Wilhelm <dewilhelm@gmail.com>
2019-12-13 22:15:25 -0500

    add fish and fisher config

---
+++ config.fish
@@ -0,0 +1,10 @@
+set -x EDITOR '/usr/bin/vim'
+set SPACEFISH_PROMPT_SEPARATE_LINE false
+set fish_greeting
+set SPACEFISH_PROMPT_ORDER host time user dir git package jobs char
+set SPACEFISH_DIR_TRUNC 10
+#set SPACEFISH_DIR_PREFIX
+set SPACEFISH_GIT_PREFIX
+set SPACEFISH_PACKAGE_PREFIX
+set SPACEFISH_CHAR_SYMBOL ':<'
+bind \t forward-char
